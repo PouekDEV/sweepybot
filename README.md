c737c2e2c269e84baca5bc3dbca5127b3981105b
Jakub Starostecki <jakub.starostecki27@gmail.com>
2024-06-28 17:30:42 +0200

    a small check up

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# sweepybot
-Sweepy is a cleaning discord bot that allows you to clean messages easily!
+# Sweepy
+Sweepy is a bot that allows you to clean messages in a bulk easily!
 
+Helping since June 25, 2020, y.
 
-<p style="text-align:center;"><big><big>Sweepy</big></big></p>
 <a href="https://top.gg/bot/725629337046614027">
-    <p style="text-align:center;"><img src="https://top.gg/api/widget/725629337046614027.svg" alt="Sweepy" /></p>
+    <img src="https://top.gg/api/widget/725629337046614027.svg" alt="Sweepy" />
 </a>
--- index.js
+++ index.js
@@ -1,59 +1,63 @@
 require('dotenv').config();
-const { REST, Routes } = require('discord.js');
-const { Client, GatewayIntentBits } = require('discord.js');
+const { REST, Routes, Client, GatewayIntentBits } = require("discord.js");
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-const { AutoPoster } = require('topgg-autoposter');
-const ap = AutoPoster(process.env.TOP_GG_TOKEN, client);
-ap.on('posted', (stats) => {
-  console.log(`Posted stats to Top.gg | ${stats.serverCount} servers`)
-})
+const { AutoPoster } = require("topgg-autoposter");
+const poster = AutoPoster(process.env.TOP_GG_TOKEN, client);
+
+poster.on('posted', (stats) => {
+    console.log(`Posted stats to Top.gg | ${stats.serverCount} servers`);
+});
+
 const token = process.env.DISCORD_TOKEN;
 const client_id = process.env.CLIENT_ID;
 const commands = [
-  {
-    name: 'ping',
-    description: 'Replies with the ping of the bot',
-  },
-  {
-    name: 'help',
-    description: 'Replies with the list of commands'
-  },
-  {
-    name: 'setup',
-    description: 'Setups the bot for use in the server'
-  },
-  {
-    name: 'clean',
-    description: 'cleans messages',
-    options: [
-      {
-        required: true,
-        type: 4,
-        name: 'amount',
-        description: 'Amount of messages to delete'
-      }
-    ],
-  }
+    {
+        name: "ping",
+        description: "Replies with the ping of the bot"
+    },
+    {
+        name: "help",
+        description: "Replies with the list of commands"
+    },
+    {
+        name: "setup",
+        description: "Setups the bot for use in the server"
+    },
+    {
+        name: "clean",
+        description: "cleans messages",
+        options: [
+            {
+                required: true,
+                type: 4,
+                name: "amount",
+                description: "Amount of messages to delete"
+            }
+        ]
+    }
 ];
-const rest = new REST({ version: '10' }).setToken(token);
+
+const rest = new REST({ version: "10" }).setToken(token);
+
 (async () => {
     try {
-        console.log('Started refreshing application (/) commands.');
+        console.log("Started refreshing application (/) commands.");
         await rest.put(Routes.applicationCommands(client_id), { body: commands });
-        console.log('Successfully reloaded application (/) commands.');
+        console.log("Successfully reloaded application (/) commands.");
     } catch (error) {
         console.error(error);
     }
 })();
-client.on('ready', () => {
-    client.user.setPresence({ activities: [{ name: '/help | Slash commands are live' }], status: 'online', type: 'PLAYING'});
+
+client.on("ready", () => {
+    client.user.setPresence({ activities: [{ name: "/help" }], status: "online", type: "PLAYING"});
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('interactionCreate', async interaction => {
+client.on("interactionCreate", async interaction => {
     if (!interaction.isChatInputCommand()) return;
     if (interaction.commandName === "ping") {
-      await interaction.reply({ content: "Pong: " + Math.round(client.ws.ping) + " ms", ephemeral: true });
+        await interaction.reply({ content: "Pong: " + Math.round(client.ws.ping) + " ms", ephemeral: true });
     }
     if(interaction.commandName === "help") {
         await interaction.reply({ content: "- Type `/clean` [number] to clean the messages (example `/clean 10`).\n- Before using the bot you need to type `/setup` for bot to work properly (adds a role named Sweepy Boss that you need to execute commands)\n- Type `/ping` to get my ping", ephemeral: true });
@@ -66,8 +70,8 @@ client.on('interactionCreate', async interaction => {
     }
     if(interaction.commandName === "setup" && interaction.member.permissions.has('ADMINISTRATOR') && !interaction.member.roles.cache.find(role => role.name === "Sweepy Boss")){
         await interaction.guild.roles.create({
-            name: 'Sweepy Boss',
-            reason: 'For setup',
+            name: "Sweepy Boss",
+            reason: "For setup",
         }).catch(console.error);
         var role = interaction.guild.roles.cache.find(role => role.name === "Sweepy Boss");
         await interaction.member.roles.add(role).catch(err => console.log(err));
@@ -78,12 +82,12 @@ client.on('interactionCreate', async interaction => {
         var failed = false;
         if(array < 101){
             await interaction.channel.bulkDelete(array).catch(async err => {
-              console.log(err);
-              await interaction.reply({ content: "You can only bulk delete messages that are under 14 days old", ephemeral: true });
-              failed = true;
+                console.log(err);
+                await interaction.reply({ content: "You can only bulk delete messages that are under 14 days old", ephemeral: true });
+                failed = true;
             });
             if(!failed){
-              await interaction.reply({ content: "Deleted " + array + " messages!", ephemeral: true });
+                await interaction.reply({ content: "Deleted " + array + " messages!", ephemeral: true });
             }
         }
         else{
